70db459b3b08ec2954b3b9e733ad4c3bd7e43b33
mica99 <micabelen2010@gmail.com>
2023-06-06 12:31:40 -0300

    Se agrego funcionamiento del canvas

--- MisFuncionesJS.js
+++ MisFuncionesJS.js
@@ -1,7 +1,4 @@
-/**
- * Funcion que elige una foto de manera aleatoria y permite seleccionar el nombre correspondiente a la imagen
- * @method mostrarFotoAleatoria
- */
+
 
 
 
@@ -11,6 +8,10 @@ var imagenes = [
     {nombre: "Pino", ruta: "Imagenes/pino1.png"},
 ];
 
+/**
+ * Funcion que elige una foto de manera aleatoria y permite seleccionar el nombre correspondiente a la imagen
+ * @method mostrarFotoAleatoria
+ */
 function mostrarFotoAleatoria() {
     var imagenSeleccionada = document.getElementById("imagen-seleccionada");
     var opciones = document.getElementsByClassName("opciones")[0].getElementsByTagName("button");
@@ -24,16 +25,22 @@ function mostrarFotoAleatoria() {
         opciones[i].addEventListener("click", function () {
             var opcionSeleccionada = this.textContent;
 
+            console.log("Opcion seleccionada: "+opcionSeleccionada)
+            console.log("Nombre imagen: "+imagenAleatoria.nombre)
             if (opcionSeleccionada === imagenAleatoria.nombre) {
                 resultado.textContent = "¡Correcto!";
+
             } else {
                 resultado.textContent = "Incorrecto. Intenta nuevamente.";
+
+
 
             }
 
             mostrarFotoAleatoria();
         });
     }
+    alert(resultado.textContent);
 }
 
 mostrarFotoAleatoria();
@@ -45,15 +52,62 @@ mostrarFotoAleatoria();
  */
 
 function calcularDivision() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var resultado = document.getElementById("total");
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const resultado = document.getElementById("total").value;
 
-    if (num2 !== 0) {
-        var division = Math.floor(num1 / num2);
-        resultado.value = division;
-    } else {
-        alert("Error: No se puede dividir por cero");
-        resultado.value = "";
+    if(num1>70000){
+        alert("No se puede realizar el grafico");
+        document.getElementById("num1").value = "";
     }
+    if(num2==0){
+        alert("Error: No se puede dividir por cero");
+        document.getElementById("total").value = "";
+    }
+    if (num1 && num2) {
+        const division = Math.floor(num1 / num2);
+        document.getElementById("total").value = division;
+        dibujarArboles(num1, division);
+    }
+}
+
+/**
+ * Funcion que elige una foto de manera aleatoria y permite seleccionar el nombre correspondiente a la imagen
+ * @method dibujarArboles
+ * @param {number} hect - contiene el tamaño del terreno
+ * @param {number} cantArb - Contiene la cantidad de arboles
+ */
+function dibujarArboles(hect, cantArb){
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+
+    console.log("Hectareas totales: "+hect)
+    console.log("Cantidad de Arboles: "+cantArb)
+    const cantCampos = hect/10000;
+    console.log("Hectareas calculadas: "+ cantCampos);
+
+    let margen=0
+    let cantArbxHec = parseInt(cantArb/cantCampos);
+
+    ctx.font="10pt Verdana";
+
+    for(let i=0;i<cantCampos;i++){
+        ctx.beginPath();
+        ctx.fillStyle = "#5bcc25";
+        ctx.fillRect(margen, 0, 100, 100);
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.fillStyle = "#333";
+        ctx.fillText( cantArbxHec+" Arboles", margen+10, 50);
+        ctx.fill();
+        ctx.closePath();
+
+        margen = margen +110;
+    }
+
+
 }
