be6b4f01e76e0d44d7b7e735445e2e8c0eb3a570
mica99 <micabelen2010@gmail.com>
2024-07-17 17:08:43 -0300

    se agregaron correciones

--- MisFuncionesJS.js
+++ MisFuncionesJS.js
@@ -13,11 +13,11 @@ var imagenes = [
  * @method mostrarFotoAleatoria
  */
 function mostrarFotoAleatoria() {
-    var imagenSeleccionada = document.getElementById("imagen-seleccionada");
-    var opciones = document.getElementsByClassName("opciones")[0].getElementsByTagName("button");
-    var resultado = document.getElementById("resultado");
+    let imagenSeleccionada = document.getElementById("imagen-seleccionada");
+    let opciones = document.getElementsByClassName("opciones")[0].getElementsByTagName("button");
+    let resultado = document.getElementById("resultado");
 
-    var imagenAleatoria = imagenes[Math.floor(Math.random() * imagenes.length)];
+    let imagenAleatoria = imagenes[Math.floor(Math.random() * imagenes.length)];
     imagenSeleccionada.src = imagenAleatoria.ruta;
     imagenSeleccionada.alt = imagenAleatoria.nombre;
 
@@ -40,7 +40,8 @@ function mostrarFotoAleatoria() {
 
 }
 
-mostrarFotoAleatoria();
+
+
 
 
 /**
@@ -53,20 +54,38 @@ function calcularDivision() {
     const num2 = parseFloat(document.getElementById("num2").value);
     const resultado = document.getElementById("total").value;
 
-    if(num1>70000){
+    const canvas = document.getElementById("myCanvas");
+
+
+    if (num1 < 0 || num2 < 0) {
+        alert("Error: No se pueden ingresar valores negativos");
+        document.getElementById("num1").value = "";
+        document.getElementById("num2").value = "";
+        document.getElementById("total").value = "";
+        canvas.width = canvas.width;
+        return; // Detener la ejecución de la función si se ingresaron valores negativos
+    }
+
+    if (num1 > 70000) {
         alert("No se puede realizar el grafico");
         document.getElementById("num1").value = "";
+        canvas.width = canvas.width;
     }
-    if(num2==0){
+
+    if (num2 == 0) {
         alert("Error: No se puede dividir por cero");
         document.getElementById("total").value = "";
+        canvas.width = canvas.width;
     }
+
     if (num1 && num2) {
         const division = Math.floor(num1 / num2);
         document.getElementById("total").value = division;
         dibujarArboles(num1, division);
+
     }
 }
+
 
 /**
  * Funcion que elige una foto de manera aleatoria y permite seleccionar el nombre correspondiente a la imagen
@@ -74,37 +93,34 @@ function calcularDivision() {
  * @param {number} hect - contiene el tamaño del terreno
  * @param {number} cantArb - Contiene la cantidad de arboles
  */
-function dibujarArboles(hect, cantArb){
+function dibujarArboles(hect, cantArb) {
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
 
     canvas.width = canvas.width;
 
-    console.log("Hectareas totales: "+hect)
-    console.log("Cantidad de Arboles: "+cantArb)
-    const cantCampos = hect/10000;
-    console.log("Hectareas calculadas: "+ cantCampos);
+    console.log("Hectareas totales: " + hect)
+    console.log("Cantidad de Arboles: " + cantArb)
+    const cantCampos = hect / 10000;
+    console.log("Hectareas calculadas: " + cantCampos);
+    let margen = 0;
+    let cantArbxHec = parseInt(cantArb / cantCampos);
+    ctx.font = "10pt Verdana";
 
-    let margen=0
-    let cantArbxHec = parseInt(cantArb/cantCampos);
+    for (let i = 0; i < cantCampos; i++) {
+        setTimeout(() => {
+            ctx.beginPath();
+            ctx.fillStyle = "#5bcc25";
+            ctx.fillRect(margen, 0, 100, 100);
+            ctx.fill();
+            ctx.closePath();
 
-    ctx.font="10pt Verdana";
-
-    for(let i=0;i<cantCampos;i++){
-        ctx.beginPath();
-        ctx.fillStyle = "#5bcc25";
-        ctx.fillRect(margen, 0, 100, 100);
-        ctx.fill();
-        ctx.closePath();
-
-        ctx.beginPath();
-        ctx.fillStyle = "#333";
-        ctx.fillText( cantArbxHec+" Arboles", margen+10, 50);
-        ctx.fill();
-        ctx.closePath();
-
-        margen = margen +110;
+            ctx.beginPath();
+            ctx.fillStyle = "#333";
+            ctx.fillText(cantArbxHec + " Arboles", margen + 10, 50);
+            ctx.fill();
+            ctx.closePath();
+            margen = margen + 110;
+        }, i * 1000);
     }
-
-
 }
